0d43464108e540227c40934df8a1fb55ae108b48
Randall Spears <rspears690@gmail.com>
2016-07-26 15:22:07 -0400

    added weather

--- main.js
+++ main.js
@@ -17,3 +17,16 @@ function updateClock () {
 
     document.getElementById("clock").firstChild.nodeValue = currentTimeString;
 }
+
+function getWeather () {
+    var location = {city: "Lubbock", state: "TX"};
+    var wgAPIKey = "e64c663775a23710";
+    var wunderURL = "http://api.wunderground.com/api/" + wgAPIKey + "/conditions/q/" + location.state + "/" + location.city + ".json";
+    $.getJSON(wunderURL, function(data) {
+      var currTemp = data.current_observation.temp_f;
+      var currConditions = data.current_observation.weather;
+      var currCity = data.current_observation.display_location.full;
+      var weatherString = currConditions + ", " + Math.round(currTemp) + "\u00B0" + "F"
+      document.getElementById("weather").firstChild.nodeValue = currCity + ": " + weatherString
+    });
+}
